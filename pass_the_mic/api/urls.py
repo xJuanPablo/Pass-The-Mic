ccfeb11bbe365d758fd46c0972bfc4f881d5482c
Juan Pablo Archuleta <pabloarchuleta@gmail.com>
2023-11-11 15:38:50 -0600

    Added Django and Virtual Environment

---
+++ pass_the_mic/api/urls.py
@@ -0,0 +1,9 @@
+# This file returns the main function if the route handler is empty
+
+from django.urls import path
+#This line imports the main function from the views.py in the api directory
+from .views import main
+
+urlpatterns = [
+    path('', main),
+]
